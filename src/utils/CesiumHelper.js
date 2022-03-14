af6d2b2162b6c8820d2f047fa2fc5a0a2a7ee76f
wuzs <1696646305@qq.com>
2022-03-14 20:52:30 +0800

    Cesium

--- src/utils/CesiumHelper.js
+++ src/utils/CesiumHelper.js
@@ -1,9 +1,35 @@
 
-class CesiumHelper{
 
+let _cfg;
+let _viewer;
+export class CesiumHelper{
+
+  //加载配置文件
+  static set config(data){
+    _cfg = data
+  }
+
+  static get config(){
+    return _cfg
+  }
+
+  static set viewer(viewer){
+    _viewer = viewer
+  }
+  static get viewer(){
+    return _viewer
+  }
+
+  /**
+   * 初始化球控件
+   * @param target
+   * @param option
+   */
+  static initMap(target, option){
+
+  }
 
 
 }
 
 
-export CesiumHelper;
