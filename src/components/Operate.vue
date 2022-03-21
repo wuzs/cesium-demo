7ec8d882a0eb3bafbd40fd09f6b7f3b133931bc0
wuzs <1696646305@qq.com>
2022-03-21 17:26:41 +0800

    Operate

---
+++ src/components/Operate.vue
@@ -0,0 +1,29 @@
+<template>
+  <div >
+      <div class="operate"></div>
+
+  </div>
+
+</template>
+
+<script>
+  export default{
+    name:"Operate",
+    data(){
+      return {}
+    }
+  }
+</script>
+
+<style>
+  .operate {
+    width:6.25rem;
+    height: 31.25rem;
+    position: absolute;
+    background:red;
+    margin-top: 10px;
+    margin-left: 10px;
+
+
+  }
+</style>
